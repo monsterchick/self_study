37a2c92e1c522eb637a9d01727d47a4d49f83ed6
kwan <kwanho.app@gmail.com>
2023-03-10 01:23:37 +0000

    fix bugs

--- sideProject/openmojiSpider/main.py
+++ sideProject/openmojiSpider/main.py
@@ -9,6 +9,27 @@ response = requests.get(url)
 text = response.text
 srcList = re.findall(r'data\Wsrc="/.*?/.*?/.*?/.*?.svg" alt=".*?"', text)
 
+
+def mkdir(path):
+    # go to Desktop directory
+    os.chdir(os.path.join(os.path.expanduser("~"), "Desktop"))
+    # check if folder exists, if not, make one. if exists, prompt a message
+    if not os.path.isdir("image"):
+        os.makedirs(path)  # make a folder called image
+        print("folder created successfully!")
+    else:
+        print("folder existed!")
+        pass
+
+
+mkdir("/Users/monstermac/Desktop/image")
+def downloadImage(image, imageName):
+    # download images with svg format
+    print("downloading {img}...".format(img=imageName))
+    resImage = requests.get(image)
+    with open(os.getcwd() + "/image" + "/" + imageName, 'wb') as file:  # open the path of image file
+        file.write(resImage.content)
+    file.close()
 for src in srcList:
     # get path of image
     srcClip = src.split("\"")[1]
@@ -22,20 +43,6 @@ for src in srcList:
     imageName = "{num}_{nme}_{cd}.svg".format(num=count, nme=nameClip, cd=codeClip)
     # print(imageName)ttt
     count += 1
+    downloadImage(image,imageName)
+print("all done. Congratulations!")
 
-    # go to Desktop directory
-    os.chdir(os.path.join(os.path.expanduser("~"), "Desktop"))
-    # check if folder exists, if not, make one. if exists, prompt a message
-    if not os.path.isdir("image"):
-        os.makedirs(os.getcwd() + "/image")  # make a folder called image
-        print("folder created successfully!")
-    else:
-        print("folder existed!")
-        pass
-
-    # download images with svg format
-    print("downloading {img}...".format(img=imageName))
-    resImage = requests.get(image)
-    with open(os.getcwd() + "/image" + "/" + imageName, 'wb') as file:  # open the path of image file
-        file.write(resImage.content)
-    file.close()
