3acffcb286eec3f3b90d9c72f10aced22cb38cb2
kwan <kwanho.app@gmail.com>
2023-03-10 01:02:38 +0000

    update sideProject

--- sideProject/openmojiSpider/main.py
+++ sideProject/openmojiSpider/main.py
@@ -1,16 +1,41 @@
+import os
 import requests
 import re
-a = "https://openmoji.org/"
+
+count = 1
+fontUrl = "https://openmoji.org"
 url = "https://openmoji.org/library/"
 response = requests.get(url)
 text = response.text
-# print(text)
-image = re.findall('<img class="emoji-variant-color lazyload astro-JSGNIHCK loaded".*? alt=',text)
-print(image)
-for i in text:
-    print(i)
+srcList = re.findall(r'data\Wsrc="/.*?/.*?/.*?/.*?.svg" alt=".*?"', text)
 
-# '<img class="emoji-variant-color \
-# lazyload astro-JSGNIHCK loaded __web-inspector-hide-shortcut__" \
-# data-src="/data/color/svg/1F4A2.svg" alt="&quot;anger symbol&quot;-emoji" \
-# data-ll-status="loaded" src="/data/color/svg/1F4A2.svg">')
+for src in srcList:
+    # get path of image
+    srcClip = src.split("\"")[1]
+    image = fontUrl + srcClip
+    # get name of image
+    nameClip = src.split("=")[2].split(";")[1]
+    # get code of image
+    codeClip = src.split(".")[0].split("/")[4]
+    # print(codeClip)
+    # print(os.path.join(os.path.expanduser("~"), "Desktop/image"))
+    imageName = "{num}_{nme}_{cd}.svg".format(num=count, nme=nameClip, cd=codeClip)
+    # print(imageName)ttt
+    count += 1
+
+    # go to Desktop directory
+    os.chdir(os.path.join(os.path.expanduser("~"), "Desktop"))
+    # check if folder exists, if not, make one. if exists, prompt a message
+    if not os.path.isdir("image"):
+        os.makedirs(os.getcwd() + "/image")  # make a folder called image
+        print("folder created successfully!")
+    else:
+        print("folder existed!")
+        pass
+
+    # download images with svg format
+    print("downloading {img}...".format(img=imageName))
+    resImage = requests.get(image)
+    with open(os.getcwd() + "/image" + "/" + imageName, 'wb') as file:  # open the path of image file
+        file.write(resImage.content)
+    file.close()
